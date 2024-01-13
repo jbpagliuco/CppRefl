622a55e3d49b0b6cd215da0dc99ed0f68427dbae
Joe Pagliuco <jbpagliuco@gmail.com>
2024-01-12 23:40:52 -0600

    Separate FileObjects file.

--- CppRefl.CodeGeneration/ICodeGeneratorExtension.cs
+++ CppRefl.CodeGeneration/ICodeGeneratorExtension.cs
@@ -3,32 +3,6 @@ using CppRefl.CodeGeneration.Reflection;
 
 namespace CppRefl.CodeGeneration
 {
-	public record FileObjects
-	{
-		/// <summary>
-		/// List of reflected classes in the given file.
-		/// </summary>
-		public IEnumerable<ClassInfo> Classes { get; init; } = Enumerable.Empty<ClassInfo>();
-
-		/// <summary>
-		/// List of reflected enums in the given file.
-		/// </summary>
-		public IEnumerable<EnumInfo> Enums { get; init; } = Enumerable.Empty<EnumInfo>();
-
-		/// <summary>
-		/// List of reflected aliases in the given file.
-		/// </summary>
-		public IEnumerable<AliasInfo> Aliases { get; init; } = Enumerable.Empty<AliasInfo>();
-
-		/// <summary>
-		/// List of reflected functions in the given file.
-		/// </summary>
-		public IEnumerable<FunctionInfo> Functions { get; init; } = Enumerable.Empty<FunctionInfo>();
-
-		public bool Any() => Classes.Any() || Enums.Any() || Aliases.Any() || Functions.Any();
-	}
-
-
 	public interface ICodeGeneratorExtension
 	{
 		/// <summary>
